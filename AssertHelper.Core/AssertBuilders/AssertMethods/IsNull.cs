56ad10acf21c77f1060dfc3dc51f4b34e83db564
Daniel Rose <danrose@gmx.de>
2015-04-28 09:13:30 +0200

    Fixed handling of Nullable<T> values

--- AssertHelper.Core/AssertBuilders/AssertMethods/IsNull.cs
+++ AssertHelper.Core/AssertBuilders/AssertMethods/IsNull.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Linq.Expressions;
 using AssertHelper.Core.AssertBuilders.AssertMethods.Base;
+using AssertHelper.Core.Extensions;
 
 namespace AssertHelper.Core.AssertBuilders.AssertMethods
 {
-    internal class IsNull : UnaryAssertMethodBase
+    internal class IsNull : AssertMethodBase, IUnaryAssertMethod
     {
         public IsNull(Type assertType)
             : base(assertType, "IsNull", new[] { typeof(object), typeof(string) })
         { }
+
+        public Expression<Action> Assert(Expression expression, string lambda)
+        {
+            return GetExpression(Expression.Convert(expression, typeof(object)), lambda.ToConstantExpression());
+        }
     }
 }
